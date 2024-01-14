48ea1a8fde2b203f63ecb7ba62b424e4a9639d33
Oz0neX <kingyasir8327@gmail.com>
2024-01-14 06:03:08 -0800

    Update

--- client/src/pages/Home/filehandle.py
+++ client/src/pages/Home/filehandle.py
@@ -1,55 +1,54 @@
-from flask import Flask, request
-import PyPDF2
+from flask import Flask, request, jsonify
+from PyPDF2 import PdfReader
 import os
+import time
 
 # import necessary packages
 
 
 app = Flask(__name__)
 
-def pdf_to_text(pdf_path):
+
+def convert_pdf_to_text(pdf_file):
     text = ""
 
-    with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
+    pdf_reader = PdfReader(pdf_file)
 
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
 
     return text
-
+def test():
+    result_text = convert_pdf_to_text("C:/Users/kingy/OneDrive/Desktop/SBHacks/sbhacksX/client/src/pages/Yasir_Whites_Resume-1.pdf")
+    print(result_text)
 
 @app.route('/uploadFile', methods=['GET', 'POST'])
 def upload_file():
     file = request.files['file']
-    file.save(os.path.join(os.getcwd(), file.filename))
+    file.save(os.path.join("C:/Users/kingy/OneDrive/Desktop/SBHacks/sbhacksX/client/src/pages/", file.filename))
     major = request.form['major']
     name = request.form['name']
-
-    # formData.append('fileName', file.name);
-    # formData.append('name', event.target.name.value);
-    # formData.append('major', event.target.major.value);
-
+    time.sleep(5)
 
     # Replace 'your_pdf_[file.pdf' with the actual path to your PDF file
-    pdf_path = file.filename
-    result_text = pdf_to_text(pdf_path)
-    
+    result_text = convert_pdf_to_text("C:/Users/kingy/OneDrive/Desktop/SBHacks/sbhacksX/client/src/pages/" + file.filename)
+    print(result_text)
 
     with open('output.txt', 'w') as output_file:
         output_file.write(result_text)
 
     # Delete the original PDF file
-    os.remove(pdf_path)
-
-    return result_text
+    response = jsonify({'some': 'data'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 200
 
 if __name__ == '__main__':
     app.run()
 
 
-
+time.sleep(5)
+test()
 
  
 # divison of labor: 
